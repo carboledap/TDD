0f3ad14815f7d5024a212963f3c49dd948c48742
Alexander Hernandez Casas <Ahernandezc@intergrupo.com>
2019-10-18 13:37:24 -0500

    Refactorizar la función y lanzar nuevamen la test

--- EmailValidator/EmailValidator.cs
+++ EmailValidator/EmailValidator.cs
@@ -5,11 +5,11 @@ namespace EmailValidator
 {
     public class EmailValidator
     {
+        public const string CorreoElectronico = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z\.!#$%&'*+/=?^_`{|}~-]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[-0-9a-z_])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([-0-9a-z_][-0-9a-z_]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+
         public static bool ValidateEmail(string text)
         {
-            string CorreoElectronico = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z\.!#$%&'*+/=?^_`{|}~-]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[-0-9a-z_])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([-0-9a-z_][-0-9a-z_]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
-
-            return Regex.IsMatch(text, CorreoElectronico);
+            return text != null && Regex.IsMatch(text, CorreoElectronico);
         }
     }
 }
