afb519011250fc8d47a0785baf37032c101f8d69
Xandher <ahernandezc@intergrupo.com>
2019-10-28 08:12:54 -0500

    Nuevas pruebas para EmailValidator

--- EmailValidator.Test/EmailValidatorTest.cs
+++ EmailValidator.Test/EmailValidatorTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EmailValidator.Test
 {
@@ -14,6 +13,45 @@ namespace EmailValidator.Test
 
             //Act
             var result = EmailValidator.ValidateEmail("ahernandezc@intergrupo.com");
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void CorreoConCaracteresInvalidos()
+        {
+            //Arrange
+            bool expected = false;
+
+            //Act
+            var result = EmailValidator.ValidateEmail("ahernandezc/#)(=*¨][@intergrupo.com");
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void CorreoNulo()
+        {
+            //Arrange
+            bool expected = false;
+
+            //Act
+            var result = EmailValidator.ValidateEmail(null);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void CorreoVacio()
+        {
+            //Arrange
+            bool expected = false;
+
+            //Act
+            var result = EmailValidator.ValidateEmail("");
 
             //Assert
             Assert.AreEqual(expected, result);
